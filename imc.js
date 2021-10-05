aaca904a42e05428d7257ec4e04927acecb10b63
Fabrício Gusmão <fabriciogusmao16@gmail.com>
2021-10-04 21:58:26 -0300

    Exercício IMC Programação Imperativa

---
+++ imc.js
@@ -0,0 +1,13 @@
+var nome = "Carlos de Souza";
+let idade = 28;
+let peso = 80.1;
+var altura = 1.76;
+var plano = true;
+
+var quadradoAltura = (altura*altura)
+ imc = peso/quadradoAltura;
+
+console.log(nome +" " + 'tem '  + idade +" "+ "e seu índice de massa corporal é"  + " " + imc);
+
+
+
